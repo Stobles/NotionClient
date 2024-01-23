765d6fc01eb05d1f47de8edc94d828f8634ea445
Stobles <82175280+Stobles@users.noreply.github.com>
2024-01-23 21:54:06 +0300

    chore: Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:4000/:path*",
+        destination: "https://api.stoble.ru/:path*",
       },
     ];
   },
