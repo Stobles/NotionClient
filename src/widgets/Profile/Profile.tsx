2270e16f63fca1fa0e2c5916bdf23c4019f587fd
Stobles <ilovespnjensen@inbox.ru>
2024-02-01 14:45:40 +0300

    feat(feature/profile): add empty profile component

--- src/widgets/Profile/Profile.tsx
+++ src/widgets/Profile/Profile.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useSessionQuery } from "@/entities/session/api/sessionApi";
+import { useLogout } from "@/features/auth";
+import {
+  DropdownMenu,
+  DropdownMenuLabel,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/shared/UI/DropdownMenu";
+import { UserAvatar } from "@/shared/UI/UserAvatar";
+
 export const Profile = () => {
-  return <div>Profile</div>;
+  const { data: session } = useSessionQuery();
+  const {} = useLogout();
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger>
+        <UserAvatar src={session?.avatar} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>stoble@mail.ru</DropdownMenuLabel>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
