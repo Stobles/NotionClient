c16a54e1cf8d41a702dd8913ce98fba124b4468d
Stobles <ilovespnjensen@inbox.ru>
2024-02-28 12:15:23 +0300

    refactor: move list of documents to separate file

--- src/widgets/DocumentsList/DocumentsList.tsx
+++ src/widgets/DocumentsList/DocumentsList.tsx
@@ -1,35 +1,16 @@
-import { useDocumentsByParentQuery } from "@/entities/document";
-import { DocumentItem } from "./UI/DocumentItem";
 import { useCreateDocument } from "@/features/documents/create";
 import { PlusIcon } from "lucide-react";
 import { ListItem } from "@/shared/UI/ListItem";
+import { DocumentsAll } from "./UI/DocumentsAll";
+import { DocumentsFavorite } from "./UI/DocumentsFavorite";
 
 export const DocumentsList = () => {
-  const { data: documents } = useDocumentsByParentQuery({ parentId: "null" });
-
   const { create } = useCreateDocument();
 
   return (
     <div>
-      {!documents ? (
-        <div className="flex flex-col gap-2">
-          <DocumentItem.Skeleton level={1} />
-          <DocumentItem.Skeleton level={1} />
-          <DocumentItem.Skeleton level={1} />
-        </div>
-      ) : (
-        <ul className="py-1 pr-1 overflow-y-auto overflow-x-hidden max-h-[430px]">
-          {documents?.map((document) => (
-            <DocumentItem
-              key={document.id}
-              id={document.id}
-              title={document.title}
-              icon={document.icon}
-            />
-          ))}
-        </ul>
-      )}
-
+      <DocumentsFavorite />
+      <DocumentsAll />
       <div onClick={() => create({ title: "Untitled" })}>
         <ListItem Icon={PlusIcon} title="Add a page" />
       </div>
