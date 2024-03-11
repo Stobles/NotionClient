7a1c0327b864c1fbb3c29283874c845b95d132dd
Stobles <ilovespnjensen@inbox.ru>
2024-03-11 17:01:01 +0300

    feat: add editor functionality and add it to page

--- src/views/Document/UI/Document.tsx
+++ src/views/Document/UI/Document.tsx
@@ -5,14 +5,19 @@ import { useDocumentByIdQuery } from "@/entities/document";
 import { EmojiPopover } from "@/features/documents/update/UI/EmojiPopover";
 import Image from "next/image";
 import { UploadCoverButton } from "./UploadCoverButton";
-import { useEffect } from "react";
+import { Editor } from "@/widgets/Editor";
+import { useUpdateDocument } from "@/features/documents/update";
+import { useDebounce } from "@/shared/hooks/useDebounce";
+import { useEffect, useState } from "react";
+import { DocumentEditor } from "./DocumentEditor";
 
 export const Document = ({ id }: { id: string }) => {
-  const { data: document } = useDocumentByIdQuery(id);
+  const { data: document, isFetched } = useDocumentByIdQuery(id);
 
   const title = document?.title || "";
 
   const isCoverImage = !!document?.coverImage;
+
   return (
     <div className="w-full h-full">
       {isCoverImage && (
@@ -27,9 +32,11 @@ export const Document = ({ id }: { id: string }) => {
           />
         </div>
       )}
-      <div className={`relative w-full max-w-[710px] h-full mx-auto z-50 px-2`}>
+      <div
+        className={`relative flex flex-col items-center w-full max-w-[800px] h-full mx-auto z-50 px-2`}
+      >
         <div
-          className={`flex flex-col gap-2 justify-end group -translate-y-10 ${
+          className={`flex flex-col gap-2 w-full max-w-[690px] justify-end group -translate-y-10 ${
             isCoverImage ? "h-[150px]" : "h-[250px]"
           }`}
         >
@@ -38,6 +45,11 @@ export const Document = ({ id }: { id: string }) => {
             <UploadCoverButton documentId={id} />
           </div>
           {title && <DocumentTitle id={id} title={title} />}
+        </div>
+        <div className="w-full">
+          {isFetched && (
+            <DocumentEditor id={id} content={document?.content || ""} />
+          )}
         </div>
       </div>
     </div>
--- src/widgets/Editor/Editor.tsx
+++ src/widgets/Editor/Editor.tsx
@@ -1,9 +1,33 @@
+"use client";
+
+import { PartialBlock } from "@blocknote/core";
 import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
 
-export const Editor = () => {
-  const editor = useCreateBlockNote();
+export const Editor = ({
+  initialContent,
+  editable,
+  onChange,
+}: {
+  initialContent: string;
+  editable: boolean;
+  onChange: (content: string) => void;
+}) => {
+  const editor = useCreateBlockNote({
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+  });
 
-  return <BlockNoteView editor={editor} />;
+  return (
+    <BlockNoteView
+      editor={editor}
+      theme="light"
+      editable={editable}
+      onChange={() => {
+        onChange(JSON.stringify(editor.document));
+      }}
+    />
+  );
 };
